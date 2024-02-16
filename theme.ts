cefed1cbb8d961f357943085cd3f7c09c3bb95a6
Kelechi <laviedegeorge1292@gmail.com>
2024-02-16 22:57:55 +0100

    init

---
+++ theme.ts
@@ -0,0 +1,17 @@
+"use client";
+import { Montserrat } from "next/font/google";
+import { createTheme } from "@mui/material/styles";
+
+const roboto = Montserrat({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const theme = createTheme({
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
+});
+
+export default theme;
